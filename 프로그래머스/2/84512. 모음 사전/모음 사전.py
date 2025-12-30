b6764a42235e36a4a536dcec3e5001f0f136fa00
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2025-12-31 01:19:40 +0900

    [level 2] Title: 모음 사전, Time: 1.09 ms, Memory: 9.43 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,13 @@
+from itertools import product
+
+def solution(word):
+    words = []
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    
+    for i in range(1, 6):
+        for p in product(vowels, repeat=i):
+            words.append(''.join(p))
+            
+    words.sort()
+    
+    return words.index(word) + 1
