93d3c951f2405cd7eb4ba2e848694914ae2928bc
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2026-02-23 20:07:30 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.61 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,20 +1,19 @@
+import re
+
 def solution(new_id):
-    new_id = new_id.lower() # 1단계
+    new_id = re.sub(r'[^a-z0-9-_\.]', '', new_id.lower())
     
-    allowed = "abcdefghijklmnopqrstuvwxyz0123456789-_."
-    new_id = "".join([c for c in new_id if c in allowed]) # 2단계
+    new_id = re.sub(r'\.{2,}', '.', new_id)
     
-    while '..' in new_id:
-        new_id = new_id.replace('..', '.') # 3단계
-        
-    new_id = new_id.strip('.') # 4단계
+    new_id = re.sub(r'^\.|\.$', '', new_id)
     
     if not new_id:
-        return "aaa" # 5단계
-    
-    new_id = new_id[:15].rstrip('.') # 6단계
-    
+        new_id = "a"
+        
+    if len(new_id) >= 16:
+        new_id = new_id[:15].rstrip('.')
+
     while len(new_id) < 3:
-        new_id += new_id[-1] # 7단계
+        new_id += new_id[-1]
         
     return new_id
