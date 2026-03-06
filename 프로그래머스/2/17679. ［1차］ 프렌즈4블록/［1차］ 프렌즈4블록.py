f13e17bc179555521c6faf4f30990130452e6761
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2026-03-06 19:08:30 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 43.71 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,25 +1,29 @@
 def solution(m, n, board):
-    board = [list(row) for row in board]
+    b = [list(row) for row in board]
     answer = 0
-
+    
     while True:
-        target = set()
+        matched = set()
+        
         for i in range(m - 1):
             for j in range(n - 1):
-                if board[i][j] != '0' and board[i][j] == board[i+1][j] == board[i][j+1] == board[i+1][j+1]:
-                    target.update([(i, j), (i+1, j), (i, j+1), (i+1, j+1)])
-        
-        if not target:
+                if b[i][j] != ' ' and b[i][j] == b[i+1][j] == b[i][j+1] == b[i+1][j+1]:
+                    matched.update([(i, j), (i+1, j), (i, j+1), (i+1, j+1)])
+
+        if not matched:
             break
             
-        answer += len(target)
-        for i, j in target:
-            board[i][j] = '0'
+        answer += len(matched)
+        for i, j in matched:
+            b[i][j] = ' '
             
         for j in range(n):
-            temp = [board[i][j] for i in range(m) if board[i][j] != '0']
-            new_col = ['0'] * (m - len(temp)) + temp
-            for i in range(m):
-                board[i][j] = new_col[i]
-                
+            top = m - 1
+            for i in range(m - 1, -1, -1):
+                if b[i][j] != ' ':
+                    b[top][j] = b[i][j]
+                    if top != i:
+                        b[i][j] = ' '
+                    top -= 1
+                    
     return answer
