1bb3c9e81055db25850e678da32c0f1d51f150f3
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2026-03-06 15:26:06 +0900

    [level 2] Title: 오픈채팅방, Time: 169.53 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,16 +1,23 @@
 def solution(record):
+    user_info = {}
+    log = []
+    
+    for r in record:
+        split_r = r.split()
+        command = split_r[0]
+        user_id = split_r[1]
+        
+        if command in ("Enter", "Change"):
+            user_info[user_id] = split_r[2]
+            
+        if command in ("Enter", "Leave"):
+            log.append((command, user_id))
+            
     answer = []
-    user_db = {}
-    
-    for r in record:
-        info = r.split()
-        if info[0] in ['Enter', 'Change']:
-            user_db[info[1]] = info[2]
-    
-    for r in record:
-        info = r.split()
-        if info[0] == 'Enter':
-            answer.append(f"{user_db[info[1]]}님이 들어왔습니다.")
-        elif info[0] == 'Leave':
-            answer.append(f"{user_db[info[1]]}님이 나갔습니다.")
+    for command, user_id in log:
+        if command == "Enter":
+            answer.append(f"{user_info[user_id]}님이 들어왔습니다.")
+        elif command == "Leave":
+            answer.append(f"{user_info[user_id]}님이 나갔습니다.")
+            
     return answer
