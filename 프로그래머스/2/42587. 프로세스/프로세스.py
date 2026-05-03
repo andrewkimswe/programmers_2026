970575e134aee786454dfccf847911f5420caf16
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2026-05-03 16:14:09 +0900

    [level 2] Title: 프로세스, Time: 0.80 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,15 +1,21 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = 0
-    queue = deque([(p, i) for i, p in enumerate(priorities)])
-    while True:
-        current = queue.popleft()
-        if any(current[0] < q[0] for q in queue):
-            queue.append(current)
+    queue = deque(priorities)
+    order = 0
+    
+    while queue:
+        cur = queue.popleft()
+        
+        if queue and cur < max(queue):
+            queue.append(cur)
+            if location == 0:
+                location = len(queue) - 1
+            else:
+                location -= 1
         else:
-            answer += 1
-            if current[1] == location:
-                return answer
-
-# Time Complexity : O(n^2)
+            order += 1
+            if location == 0:
+                return order
+            else:
+                location -= 1
