3768d98784962891a003639670cd26cd41cae711
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2026-05-11 15:12:21 +0900

    [level 1] Title: 모의고사, Time: 2.33 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,23 @@
 def solution(answers):
-    answer_1 = [1,2,3,4,5]
-    answer_2 = [2,1,2,3,2,4,2,5]
-    answer_3 = [3,3,1,1,2,2,4,4,5,5]
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
     scores = [0, 0, 0]
-    
+
     for i, ans in enumerate(answers):
-        if ans == answer_1[i % len(answer_1)]:
+        if ans == p1[i % len(p1)]:
             scores[0] += 1
-        if ans == answer_2[i % len(answer_2)]:
+        if ans == p2[i % len(p2)]:
             scores[1] += 1
-        if ans == answer_3[i % len(answer_3)]:
+        if ans == p3[i % len(p3)]:
             scores[2] += 1
-    
+
     max_score = max(scores)
-    
-    result = []
-    for i, s in enumerate(scores):
-        if s == max_score:
-            result.append(i + 1)
-            
-    return result
+    answer = []
+
+    for idx, score in enumerate(scores):
+        if score == max_score:
+            answer.append(idx + 1)
+
+    return answer
