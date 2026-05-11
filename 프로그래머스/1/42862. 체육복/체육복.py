b06463bcee8f5c38485bd746385b648529a4e7ff
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2026-05-11 15:53:36 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,11 @@
 def solution(n, lost, reserve):
-    actual_lost = sorted(list(set(lost) - set(reserve)))
-    actual_reserve = set(reserve) - set(lost)
-    
-    lost_count = len(actual_lost)
-    
-    for student in actual_lost:
-        if student - 1 in actual_reserve:
-            actual_reserve.remove(student - 1)
-            lost_count -= 1
-        elif student + 1 in actual_reserve:
-            actual_reserve.remove(student + 1)
-            lost_count -= 1
-    
-    return n - lost_count
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost)
+
+    for r in sorted(reserve_set):
+        if r - 1 in lost_set:
+            lost_set.remove(r - 1)
+        elif r + 1 in lost_set:
+            lost_set.remove(r + 1)
+
+    return n - len(lost_set)
